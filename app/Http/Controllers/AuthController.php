fcbf961b59a8bf1d52e070747ba9ed103a554473
eduado <eduardo.hor@outlook.com>
2023-03-13 14:35:07 -0300

    :sparkles: feat: Adding user registration path

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+
+	public function register(UserRequest $request)
+	{
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => Hash::make($request->password)
+		]);
+
+		return $user;
+
+	}
+
 	public function login(LoginRequest $request)
 	{
 		$credentials = $request->all(['email', 'password']);
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('register' , [AuthController::class, 'register']);
 Route::post('login' , [AuthController::class, 'login']);
 
 Route::middleware('jwt.auth')->group(function(){
