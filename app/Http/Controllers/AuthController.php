597959c6303185fe44570eaa505b50065888404e
eduado <eduardo.hor@outlook.com>
2023-03-13 12:25:52 -0300

    :sparkles: feat: Adding login route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller
+{
+	public function login(LoginRequest $request)
+	{
+		$credentials = $request->all(['email', 'password']);
+
+		if (!$token = auth('api')->attempt($credentials)) {
+			return response()->json(['error' => 'Unauthorized'], 401);
+		}
+
+		return response()->json([
+			'access_token' => $token,
+			'token_type' => 'bearer',
+			'expires_in' => auth()->factory()->getTTL() * 60
+		]);
+	}
+}
